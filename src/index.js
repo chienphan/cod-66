a95e6a622123dd0dad5dd0731a8ebfbc8ade87d3
chienphan <ducchien.phan@gmail.com>
2021-11-28 23:33:50 +0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const name = 'Your name'
+
+ReactDOM.render(
+      <h1 style={{ textAlign: 'center' }}>
+		{ name }
+</h1>,
+document.getElementById('root')
+);
